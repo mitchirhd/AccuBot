bb98681693f633ab904b7dd3b49e4e6f1eefa3bf
mitchirhd <michaelreinhard1711@gmail.com>
2024-05-01 14:57:04 +0200

    implemented Login

---
+++ AccuBot/AccuBot/Controller/Logger.cs
@@ -0,0 +1,18 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccuBot.Controller
+{
+    public class ConsoleLogger
+    {
+        public static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ AccuBot/AccuBot/Program.cs
@@ -0,0 +1,22 @@
+﻿using AccuBot.Controller;
+using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+    private static DiscordSocketClient _client;
+
+    public static async Task Main()
+    {
+        _client = new DiscordSocketClient();
+
+        _client.Log += ConsoleLogger.Log;
+        var token = File.ReadAllText("token.txt");
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+}
